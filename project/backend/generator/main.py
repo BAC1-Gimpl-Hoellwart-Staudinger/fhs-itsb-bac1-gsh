f4de50e29a247a2b18ce259b56390e7aa7dfa1cc
Clemens Gimpl <cgimpl.its-b2021@fh-salzburg.ac.at>
2023-10-16 17:44:53 +0200

    started working on generator

---
+++ project/backend/generator/main.py
@@ -0,0 +1,12 @@
+from generator import generate_dataset, dump_dataset
+from datetime import date
+
+if __name__ == "__main__":
+    num_employees = 4
+    start_date = date(2023, 1, 1)
+    end_date = date(2024, 12, 31)
+    max_vacation_days = 10
+
+    dataset = generate_dataset(num_employees, start_date, end_date, max_vacation_days)
+    dump_dataset(dataset)
+    print("Successfully generated dataset.")
