75934f4050eb71ebbd1a1ebc730c5febd01f1132
fhs48133 <jstaudinger.its-b2021@fh-salzburg.ac.at>
2023-10-17 19:34:28 +0200

    added test for creating schedules

--- project/backend/generator/main.py
+++ project/backend/generator/main.py
@@ -1,8 +1,9 @@
 from employee_generator import EmployeeGenerator
 from schedule_generator import ScheduleGenerator
-from datetime import date
+from datetime import date, datetime
 
 if __name__ == "__main__":
+    now = datetime.now()
     num_employees = 4
     start_date = date(2024, 1, 1)
     end_date = date(2024, 12, 31)
@@ -12,4 +13,10 @@ if __name__ == "__main__":
     dataset = EmployeeGenerator.generate(num_employees, start_date, end_date, min_vacation_days, max_vacation_days)
     EmployeeGenerator.dump_json(dataset=dataset)
 
-    ScheduleGenerator.generate(start_date=str(start_date), end_date=str(end_date))
+    schedule = []   # for testing --> creating 1000 schedules takes approximately 300 ms
+    for i in range(1000):
+        schedule.append(ScheduleGenerator.generate(start_date=str(start_date), end_date=str(end_date), dataset=dataset))
+
+    now1 = datetime.now()
+    print(now1-now)
+    print(len(schedule))
