87b764e7bd5005aa6b8b596393b590739bdbdc66
fhs48133 <jstaudinger.its-b2021@fh-salzburg.ac.at>
2023-10-17 19:35:04 +0200

    optimized function --> took data from main and saved vacation schedule outside of loop

--- project/backend/generator/schedule_generator.py
+++ project/backend/generator/schedule_generator.py
@@ -1,31 +1,29 @@
 import random
-import os
-import json
 from datetime import timedelta
 from helper_functions import check_holiday, string_to_date
 
 
 class ScheduleGenerator:
     @staticmethod
-    def generate(start_date, end_date, input_directory="output", file_name="dataset.json"):
-        file_path = os.path.join(input_directory, file_name)
+    def generate(start_date, end_date, dataset):
 
-        with open(file_path, "r") as json_input_file:
-            data = json.load(json_input_file)
-        employees = data['employees']
-
+        employees = dataset['employees']
         delta = timedelta(days=1)
         start__date = string_to_date(start_date)
         end__date = string_to_date(end_date)
 
         schedule = []
+        vac_schedule = []
+        for i in employees:
+            vac_schedule.append([string_to_date(date) for date in i['vacation_schedule']])
 
         while start__date <= end__date:
             start__date += delta
             tmp_employee = random.choice(employees)
-            vacation_schedule = [string_to_date(date) for date in tmp_employee['vacation_schedule']]
-            while check_holiday(start__date, vacation_schedule) is not True:
+            vac_schedule_tmp = vac_schedule[tmp_employee['id']-1]
+            while check_holiday(start__date, vac_schedule_tmp) is not True:
                 tmp_employee = random.choice(employees)
-                vacation_schedule = [string_to_date(date) for date in tmp_employee['vacation_schedule']]
+                vac_schedule_tmp = vac_schedule[tmp_employee['id']-1]
             schedule.append(tmp_employee['id'])
-        print(f'Schedule length: {len(schedule)}')
+        return schedule
+        #  print(f'Schedule length: {len(schedule)}')
