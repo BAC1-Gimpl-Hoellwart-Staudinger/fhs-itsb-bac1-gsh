ca43f6aa5b6c1ee4477655c3b14f11ac779cba58
fhs48133 <jstaudinger.its-b2021@fh-salzburg.ac.at>
2023-10-25 16:07:32 +0200

    started with genetic algorithm

--- project/backend/django_api/generator/genetic_algorithm.py
+++ project/backend/django_api/generator/genetic_algorithm.py
@@ -1,26 +1,37 @@
 from datetime import timedelta
 from .helper_functions import string_to_date,get_austrian_holidays_dates
 import numpy as np
+from .schedule_generator import ScheduleGenerator
 
 
 class GeneticAlgorithm:
     @staticmethod
-    def fitness(schedule, start_date, end_date, employees):
+    def genetic_algorithm(start_date, end_date, metadata_body):
+        population = []
+        for i in range(100000):
+            population.append(ScheduleGenerator.generate_sample_schedule(start_date,end_date,metadata_body['employees'])[0])
+        rankedschedules = []
+        for s in population:
+            rankedschedules.append((GeneticAlgorithm.fitness(s, start_date, end_date, metadata_body),s))
+        rankedschedules.sort()
+        print(rankedschedules[0][0])
+
+    @staticmethod
+    def fitness(schedule, start_date, end_date, data):
         delta = timedelta(days=1)
-        start__date = string_to_date(start_date)
-        end__date = string_to_date(end_date)
+        employees = data['employees']
+        start__date = start_date.date()
+        end__date = end_date.date()
         weekdays = []
         weekends = []
         holidays = []
         holiday_list = get_austrian_holidays_dates(start__date, end__date)
-
-        for _ in range(employees):
+        for _ in range(len(employees)):
             weekdays.append(0)
             weekends.append(0)
             holidays.append(0)
         counter = 0
         while start__date <= end__date:
-            start__date += delta
             day = start__date.weekday()
             if day == 5 or day == 6:
                 weekends[schedule[counter]-1] += 1
@@ -29,12 +40,17 @@ class GeneticAlgorithm:
             if start__date in holiday_list:
                 holidays[schedule[counter]-1] += 1
             counter += 1
+            start__date += delta
 
         mean_weekdays = np.mean(weekdays)
         mean_weekends = np.mean(weekends)
+        mean_holidays = np.mean(holidays)
         deviation_weekdays = 0
         deviation_weekends = 0
-        for s in zip(weekdays, weekends):
+        deviation_holidays = 0
+
+        for s in zip(weekdays, weekends,holidays):
             deviation_weekdays += abs(mean_weekdays - s[0])
             deviation_weekends += abs(mean_weekends - s[1])
-        return (deviation_weekdays * 10) + (deviation_weekends * 50)
+            deviation_holidays += abs(mean_holidays - s[2])
+        return (deviation_weekdays * 10) + (deviation_weekends * 50) + (deviation_holidays * 100)
