04b33a54d81ed18b74152e9df8ab567edc8dafa5
fhs48133 <jstaudinger.its-b2021@fh-salzburg.ac.at>
2023-10-28 13:09:26 +0200

    one day too much was generated --> fixed

--- project/backend/django_api/generator/schedule_generator.py
+++ project/backend/django_api/generator/schedule_generator.py
@@ -9,22 +9,27 @@ class ScheduleGenerator:
     @staticmethod
     def generate_sample_schedule(start_date, end_date, employees):
         delta = timedelta(days=1)
-
+        start__date = start_date.date()
+        end__date = end_date.date()
         execution_time_start = timer()
 
         schedule = []
         vac_schedule = []
+
         for employee in employees:
             vac_schedule.append([string_to_date(date) for date in employee['vacation_schedule']])
 
-        while start_date <= end_date:
-            start_date += delta
+        for s in vac_schedule:
+            for i in range(len(s)):
+                s[i] = s[i].date()
+        while start__date < end__date:
             employee = random.choice(employees)
             vac_schedule_tmp = vac_schedule[employee['id']-1]
             while check_holiday(start_date, vac_schedule_tmp) is not True:
                 employee = random.choice(employees)
                 vac_schedule_tmp = vac_schedule[employee['id']-1]
             schedule.append(employee['id'])
+            start__date += delta
 
         execution_time_end = timer()
         execution_time_ms = round((execution_time_end - execution_time_start) * 1000, 2)
