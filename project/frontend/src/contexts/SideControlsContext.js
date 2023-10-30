0e94e1e3c102ffa027e48abe028a68ab29172de2
Stefan Höllwart <shoellwart.its-b2021@fh-salzburg.ac.at>
2023-10-30 11:59:37 +0100

    streamlined some state

--- project/frontend/src/contexts/SideControlsContext.js
+++ project/frontend/src/contexts/SideControlsContext.js
@@ -5,7 +5,7 @@ const SideControlsContext = createContext();
 function SideControlsProvider({ children }) {
     const LOCAL_STORAGE_DATASETS_KEY = 'datasets';
     const [datasetSize, setDatasetSize] = useState(window.localStorage.getItem(LOCAL_STORAGE_DATASETS_KEY) || 0);
-    const [stats, setStats] = useState({});
+    const [stats, setStats] = useState(null);
 
     return (
         <SideControlsContext.Provider value={{
