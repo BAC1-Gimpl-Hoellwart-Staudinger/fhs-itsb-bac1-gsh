60d1092d8fc25c662838951a77aa2fc607f3739f
Stefan Höllwart <shoellwart.its-b2021@fh-salzburg.ac.at>
2023-11-14 09:59:13 +0100

    both algos now use the same fitness function

--- project/backend/django_api/generator/genetic_algorithm2.py
+++ project/backend/django_api/generator/genetic_algorithm2.py
@@ -4,6 +4,7 @@ import numpy as np
 import random
 from timeit import default_timer as timer
 from .schedule_generator import ScheduleGenerator
+from .genetic_algorithm import GeneticAlgorithm
 import random
 
 class Individual:
@@ -51,7 +52,7 @@ class GeneticAlgorithm2:
         population = []
         for _ in range(size):
             schedule = ScheduleGenerator.generate_sample_schedule(start_date, end_date, metadata_body['employees'])[0]
-            population.append(Individual(schedule, GeneticAlgorithm2.fitness(schedule, start_date, end_date, metadata_body)))
+            population.append(Individual(schedule, GeneticAlgorithm.fitness(schedule, start_date, end_date, metadata_body)))
         return population
     
     @staticmethod
@@ -99,55 +100,5 @@ class GeneticAlgorithm2:
             for i in range(len(new_schedule)):
                 if random.random() < 0.01:
                     new_schedule[i] = random.randint(1, num_employees)
-            new_population.append(Individual(new_schedule, GeneticAlgorithm2.fitness(new_schedule, start_date, end_date, metadata_body)))
+            new_population.append(Individual(new_schedule, GeneticAlgorithm.fitness(new_schedule, start_date, end_date, metadata_body)))
         return new_population
-
-
-
-    @staticmethod
-    def fitness(schedule, start_date, end_date, data):
-        delta = timedelta(days=1)
-        employees = data['employees']
-        vac_schedule = []
-        for employee in employees:
-            vac_schedule.append([string_to_date(date_) for date_ in employee['vacation_schedule']])
-        for s in vac_schedule:
-            for i in range(len(s)):
-                s[i] = s[i].date()
-        start__date = start_date.date()
-        end__date = end_date.date()
-        weekdays = []
-        weekends = []
-        holidays = []
-        holiday_list = get_austrian_holidays_dates(start__date, end__date)
-        for _ in range(len(employees)):
-            weekdays.append(0)
-            weekends.append(0)
-            holidays.append(0)
-        counter = 0
-
-        while start__date < end__date:
-            day = start__date.weekday()
-            if start__date in vac_schedule[schedule[counter] - 1]:
-                return 10000
-            if day == 5 or day == 6:
-                weekends[schedule[counter] - 1] += 1
-            else:
-                weekdays[schedule[counter] - 1] += 1
-            if start__date in holiday_list:
-                holidays[schedule[counter] - 1] += 1
-            counter += 1
-            start__date += delta
-
-        mean_weekdays = np.mean(weekdays)
-        mean_weekends = np.mean(weekends)
-        mean_holidays = np.mean(holidays)
-        deviation_weekdays = 0
-        deviation_weekends = 0
-        deviation_holidays = 0
-
-        for s in zip(weekdays, weekends, holidays):
-            deviation_weekdays += abs(mean_weekdays - s[0])
-            deviation_weekends += abs(mean_weekends - s[1])
-            deviation_holidays += abs(mean_holidays - s[2])
-        return round((deviation_weekdays * 10) + (deviation_weekends * 50) + (deviation_holidays * 100), 2)
