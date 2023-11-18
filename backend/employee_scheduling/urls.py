4cdfb46d4857e99af90ceadc6f2c406b1cd01dc0
cgimpl <clemensgimpl@gmail.com>
2023-11-18 17:03:06 +0100

    Revert "created holidays endpoint"
    This reverts commit 743206bfcc4b95e0205220c0c268b403062e8f04.
    Creating an app called "holidays" clashes with the "holidays" library we are using

--- backend/employee_scheduling/urls.py
+++ backend/employee_scheduling/urls.py
@@ -4,6 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('generator.urls')),
-    path('api/', include('stats.urls')),
-    path('api/', include('holidays.urls')),
+    path('api/', include('stats.urls'))
 ]
